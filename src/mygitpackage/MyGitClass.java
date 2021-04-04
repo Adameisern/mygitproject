6b36ba0f2f25fc179565280aa57c02ce5798cca4
Adameisern <81634842+Adameisern@users.noreply.github.com>
2021-04-04 18:13:11 +0300

    Update MyGitClass.java
    Github.com daki yeni versiyonumu oluşturuyorum.

--- src/mygitpackage/MyGitClass.java
+++ src/mygitpackage/MyGitClass.java
@@ -5,5 +5,7 @@ public class MyGitClass {
 		System.out.println("my second commit");
 		System.out.println("my third commit");
 		System.out.println("my fourth verison");
+		System.out.println("uzak sunucudaki kodum"); // github.com daki yeni kodum
+		
 	}
 }
