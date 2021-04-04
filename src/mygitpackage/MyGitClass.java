d0ee3836b3e755a82610c0637e8d124c01e50d60
Adameisern <81634842+Adameisern@users.noreply.github.com>
2021-04-04 19:20:01 +0300

    Update MyGitClass.java

--- src/mygitpackage/MyGitClass.java
+++ src/mygitpackage/MyGitClass.java
@@ -3,7 +3,7 @@ public class MyGitClass {
 	public static void main(String[] args) {
 		System.out.println("Version 1");
 		System.out.println("my second commit");
-		System.out.println("my third commit");
+		System.out.println("my third commit --- Github değişiklik");
 		System.out.println("my fourth verison");
 		System.out.println("uzak sunucudaki kodum"); // github.com daki yeni kodum
 		System.out.println("değişiklik yaptım");
